3256b39c31301341033c3133588656a732482714
Lang Sharpe <langer8191@gmail.com>
2016-08-02 17:15:35 +1000

    Nothing useful being done

--- spec/zuul/client_spec.rb
+++ spec/zuul/client_spec.rb
@@ -4,8 +4,4 @@ describe Zuul::Client do
   it 'has a version number' do
     expect(Zuul::Client::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
